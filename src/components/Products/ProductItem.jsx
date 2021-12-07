6e64fa614bf017926f89e52bc1134c0e08219b40
Pedro Copes <petercopes.96@gmail.com>
2021-12-06 23:48:24 -0300

    added image to product item

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,10 +1,11 @@
-import { Flex, Text } from "@chakra-ui/layout";
+import { Flex, Heading, Text } from "@chakra-ui/layout";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/layout";
 import ItemCounter from "./ItemCounter";
 import { useDispatch } from "react-redux";
 import { CartActions } from "../../store/cart-slice";
+import { Image } from "@chakra-ui/image";
 const ProductItem = ({ data }) => {
   const dispatch = useDispatch();
   const onAddToCart = (quantity) => {
@@ -20,29 +21,33 @@ const ProductItem = ({ data }) => {
       w={"45%"}
       h={"50%"}
       justifyContent={"space-between"}
+      direction={"column"}
     >
-      <Flex
-        direction={"column"}
-        justifyContent={"space-between"}
-        alignItems={"flex-start"}
-      >
-        <ChakraLink
-          as={Link}
-          textDecoration={"none"}
-          color={"black"}
-          to={data.id}
+      <Image src={data.imgsrc}></Image>
+      <Flex w={"100%"} m={5} justify={"space-around"}>
+        <Flex
+          direction={"column"}
+          justifyContent={"space-between"}
+          alignItems={"flex-start"}
         >
-          <h3>{data.name}</h3>
-        </ChakraLink>
-        <p>${data.price}</p>
-      </Flex>
-      <Flex direction={"column"} w={"30%"}>
-        <ItemCounter
-          buttonText={"Add to cart"}
-          limit={data.stock}
-          idProduct={data.id}
-          clickHandler={onAddToCart}
-        />
+          <ChakraLink
+            as={Link}
+            textDecoration={"none"}
+            color={"black"}
+            to={data.id}
+          >
+            <Heading size={"md"}>{data.name}</Heading>
+          </ChakraLink>
+          <Heading size={"sm"}>${data.price}</Heading>
+        </Flex>
+        <Flex direction={"column"} w={"30%"}>
+          <ItemCounter
+            buttonText={"Add to cart"}
+            limit={data.stock}
+            idProduct={data.id}
+            clickHandler={onAddToCart}
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
